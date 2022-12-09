2f9b9dd4c989f461d9501774c17600b2626acde7
lfml <lfml@cesar.org.br>
2022-12-09 16:41:36 -0300

    fix in retrieving top1 accuracy

--- trainer.py
+++ trainer.py
@@ -771,7 +771,7 @@ class Trainer:
                 optimizer.step()
                 prob_optimizer.step()
 
-            acc1, _ = accuracy(output, target, topk=(1,))
+            acc1 = accuracy(output, target, topk=(1,))
             losses.update(loss.item(), images.size(0))
             top1.update(acc1[0], images.size(0))
             # top5.update(acc5[0], images.size(0))
